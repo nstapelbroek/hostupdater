ff7a4e6187a6394fe5e2468f2a235295bd98e1b9
Nico Stapelbroek <nstapelbroek@gmail.com>
2018-07-31 21:14:01 +0200

    Introduce an interval flag to let the hostupdater run for a long time

--- cmd/traefik.go
+++ cmd/traefik.go
@@ -5,37 +5,63 @@ import (
 	"github.com/nstapelbroek/hostupdater/traefik"
 	"github.com/nstapelbroek/hostupdater/helper"
 	"github.com/Sirupsen/logrus"
-	"os"
+	"time"
+	"net"
 )
 
 func init() {
 	rootCmd.AddCommand(traefikCmd)
 	traefikCmd.Flags().String("address", "127.0.0.1", "The IP of the Traefik server we're trying to fetch the frontend configuration from.")
 	traefikCmd.Flags().Int16("port", 8080, "The port where the Traefik host is serving it's API.")
+	traefikCmd.Flags().Int8("interval", 0, "Update every X seconds, use the default value of 0 for a single execution.")
 }
 
 var traefikCmd = &cobra.Command{
 	Use:   "traefik",
 	Short: "Retrieve frontend routing information from a Traefik router",
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
 		address, _ := cmd.Flags().GetString("address")
 		port, _ := cmd.Flags().GetInt16("port")
+		interval, _ := cmd.Flags().GetInt8("interval")
+
 		traefikIp, err := helper.AddressToIp(address)
 		if err != nil {
 			logrus.WithFields(logrus.Fields{"address": address}).Error("failed resolving address to a usable IP")
-			os.Exit(1)
+			return
 		}
 
-		hosts, err := traefik.GetHosts(traefikIp, port)
-		if err != nil {
-			logrus.WithFields(logrus.Fields{"ip": traefikIp, "port": port}).Error(err)
-			os.Exit(1)
+		err = updateHostsFromTraefikApi(traefikIp, port)
+		if err != nil || interval == 0 {
+			return
 		}
 
-		err = helper.WriteHostsToFile(hosts)
-		if err != nil {
-			logrus.Errorln("failed persist changes to hostsfile.", err)
-			os.Exit(1)
+		ticker := time.NewTicker(time.Second * time.Duration(interval))
+		quit := make(chan struct{})
+		for {
+			select {
+			case <-ticker.C:
+				if err := updateHostsFromTraefikApi(traefikIp, port); err != nil {
+					return err
+				}
+			case <-quit:
+				ticker.Stop()
+				return
+			}
 		}
 	},
+}
+
+func updateHostsFromTraefikApi(traefikIp net.IP, port int16) (err error) {
+	hosts, err := traefik.GetHosts(traefikIp, port)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{"ip": traefikIp, "port": port}).Error(err)
+		return
+	}
+
+	err = helper.WriteHostsToFile(hosts)
+	if err != nil {
+		logrus.Errorln("failed persist changes to hostsfile.", err)
+	}
+
+	return
 }
