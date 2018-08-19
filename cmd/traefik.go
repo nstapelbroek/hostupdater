c6c4f7abdc55cdcbe32ff415a727678dfbcb9765
Nico Stapelbroek <nstapelbroek@gmail.com>
2018-08-19 14:34:47 +0200

    Wrap IP and port of Traefik service in a struct

--- cmd/traefik.go
+++ cmd/traefik.go
@@ -6,7 +6,6 @@ import (
 	"github.com/nstapelbroek/hostupdater/helper"
 	"github.com/sirupsen/logrus"
 	"time"
-	"net"
 )
 
 func init() {
@@ -14,7 +13,8 @@ func init() {
 	traefikCmd.Flags().String("address", "127.0.0.1", "The IP of the Traefik server we're trying to fetch the frontend configuration from.")
 	traefikCmd.Flags().Int16("port", 8080, "The port where the Traefik host is serving it's API.")
 	traefikCmd.Flags().Int8("interval", 0, "Update every X seconds, use the default value of 0 for a single execution.")
-	traefikCmd.Flags().Int8("wait", 0, "Wait an ammount of X seconds before execution, usefull when starting docker-compose files and not every service is registered")
+	traefikCmd.Flags().Int8("wait", 0, "Wait an amount of X seconds before execution allowing services to pass health-checks and register")
+	traefikCmd.Flags().String("filter", "*", "Only update the hosts who pass this regular expression, useful when using multiple loadbalancers")
 }
 
 var traefikCmd = &cobra.Command{
@@ -29,25 +29,30 @@ var traefikCmd = &cobra.Command{
 		address, _ := cmd.Flags().GetString("address")
 		port, _ := cmd.Flags().GetInt16("port")
 		interval, _ := cmd.Flags().GetInt8("interval")
+		filter, _ := cmd.Flags().GetString("filter")
 
 		time.Sleep(time.Duration(waitTime) * time.Second)
+
 		traefikIp, err := helper.AddressToIp(address)
 		if err != nil {
 			logrus.WithFields(logrus.Fields{"address": address}).Error("failed resolving address to a usable IP")
 			return
 		}
 
-		err = updateHostsFromTraefikApi(traefikIp, port)
+		TraefikAddress := traefik.Address{IP: traefikIp, PortNumber: port}
+
+		err = updateHostsFromTraefikApi(TraefikAddress, filter)
 		if err != nil || interval == 0 {
 			return
 		}
 
+		// The interval argument is set, startup the timer
 		ticker := time.NewTicker(time.Second * time.Duration(interval))
 		quit := make(chan struct{})
 		for {
 			select {
 			case <-ticker.C:
-				if err := updateHostsFromTraefikApi(traefikIp, port); err != nil {
+				if err := updateHostsFromTraefikApi(TraefikAddress, filter); err != nil {
 					return err
 				}
 			case <-quit:
@@ -58,12 +63,14 @@ var traefikCmd = &cobra.Command{
 	},
 }
 
-func updateHostsFromTraefikApi(traefikIp net.IP, port int16) (err error) {
-	hosts, err := traefik.GetHosts(traefikIp, port)
+func updateHostsFromTraefikApi(address traefik.Address, filter string) (err error) {
+	hosts, err := traefik.GetHosts(address.IP, address.PortNumber)
 	if err != nil {
-		logrus.WithFields(logrus.Fields{"ip": traefikIp, "port": port}).Error(err)
+		logrus.WithFields(logrus.Fields{"ip": address.IP, "port": address.PortNumber}).Error(err)
 		return
 	}
+
+	// Filter the results here
 
 	err = helper.WriteHostsToFile(hosts)
 	if err != nil {
